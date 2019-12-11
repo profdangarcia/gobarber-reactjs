d427756f3dfcfac85f2ed47fec5f987c32858693
Dan Garcia <daniel.of@gmail.com>
2019-12-11 10:28:27 -0300

    Private Routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     "camelcase": "off",
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+
   },
 };
