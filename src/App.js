dc50494cfb500f5bf149dbb3ffbb4b7ae92378a9
Dan Garcia <daniel.of@gmail.com>
2019-12-14 10:36:02 -0300

    User and Auth persist with Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 // Provider somente com o Redux
 import { Provider } from 'react-redux';
@@ -9,17 +10,19 @@ import Routes from './routes';
 import history from './services/history';
 
 // store é parte do redux e deve vir após as configurações do Reactotron
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
