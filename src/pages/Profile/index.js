0bfd4f4bbbead403ded9a3fdd5718b1829fd3f5e
Dan Garcia <daniel.of@gmail.com>
2019-12-20 15:29:45 -0300

    Configuring SignOut

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
+
 import { Container } from './styles';
 import AvatarInput from './AvatarInput';
 
@@ -13,6 +15,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -33,7 +39,9 @@ export default function Profile() {
         <button type="submit">Atualizar Perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
