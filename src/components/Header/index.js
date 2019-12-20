53d97e91c6ce795244d8c6a97a31ef0f0de2f274
Dan Garcia <daniel.of@gmail.com>
2019-12-20 13:44:09 -0300

    Header modification to use Redux reducer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
@@ -8,6 +9,8 @@ import defaultAvatar from '~/assets/default-avatar.png';
 import logo from '~/assets/logo-purple.svg';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -19,10 +22,10 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Daniel Garcia</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
-            <img src={defaultAvatar} alt="Daniel Garcia" />
+            <img src={profile.avatar.url || defaultAvatar} alt="User Avatar" />
           </Profile>
         </aside>
       </Content>
